c308bb67fc177661d1cd6a6965a8e8115e42945b
Fx Suarez <francoisxaviersuarez@gmail.com>
2020-05-26 14:21:59 +0200

    split fade_worker in external file

---
+++ fade_worker.py
@@ -0,0 +1,15 @@
+class FadeWorker(Thread):
+    
+    def __init__(self, listener, delay):
+        Thread.__init__(self)
+        self.delay=delay
+        self.listener=listener
+        
+    def run(self):
+        self.go=True
+        while self.go:
+            self.listener.fade()
+            time.sleep(self.delay/1000.)
+
+    def stop(self):
+        self.go=False
